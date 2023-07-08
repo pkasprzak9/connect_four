05da864d31181c0486874567feb08aa9bdd6ea92
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-08 21:57:41 +0200

    Move main to game dir

--- game/main.rb
+++ game/main.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: false
 
-require_relative './lib/board'
-require_relative './lib/game'
+require_relative '../lib/board'
+require_relative '../lib/game'
 
 game = Game.new(Board.new)
 game.play_game
